26c0dd58e31aacc2ff81fd9ae9307d65dfed8485
Trevor Scheer <trevor.scheer@gmail.com>
2026-04-28 22:49:13 -0700

    add vue-app example (Vue 3 + @vue/apollo-composable, .vue SFC extraction)

--- eslint.config.js
+++ eslint.config.js
@@ -14,6 +14,7 @@ const config = [
       "**/generated/**",
       "**/prisma/migrations/**",
       "**/next-env.d.ts",
+      "**/.svelte-kit/**",
     ],
   },
 
---
+++ examples/vue-app/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import PokedexList from "./components/PokedexList.vue";
+</script>
+
+<template>
+  <main style="font-family: system-ui, sans-serif; max-width: 900px; margin: 0 auto; padding: 24px">
+    <PokedexList />
+  </main>
+</template>
